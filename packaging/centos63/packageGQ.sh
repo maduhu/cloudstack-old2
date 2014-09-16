ad36f548933daa72441e3d05011aeaae27afae23
jeff <jeff@greenqloud.com>
2014-09-16 11:46:05 +0000

    add correct output to the revision

--- packaging/centos63/packageGQ.sh
+++ packaging/centos63/packageGQ.sh
@@ -42,7 +42,7 @@ fi
 
 VERSION=`(cd ../../; mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version) | grep '^[0-9]\.'`
 RELEASE_REVISION="14.09.00"
-GQREL=$TMP.${TMP2:0:8}
+GQREL=$RELEASE_REVISION
 if echo $VERSION | grep SNAPSHOT ; then
   REALVER=`echo $VERSION | cut -d '-' -f 1`
   DEFVER="-D_ver $REALVER"
