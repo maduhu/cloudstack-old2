40e1ddbfa93ac92872fcc52d7c115ed8a5016ace
jeff <jeff@greenqloud.com>
2014-09-24 10:16:53 +0000

    change an underscore to a plus for rpms

--- packaging/centos63/packageGQ.sh
+++ packaging/centos63/packageGQ.sh
@@ -55,7 +55,7 @@ if [ "$commits" -ne "0" ]; then
   # which means this is for the next version to be released. rather
   # than actually calculate the next version it is simpler (lazier) to
   # just put nextver on the line.
-  GQREL="${GQREL}_nextver"
+  GQREL="${GQREL}+nextver"
 fi
 
 if echo $VERSION | grep SNAPSHOT ; then
